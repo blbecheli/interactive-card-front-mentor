dce83e0e3b809ab9a9c9d2fb383f36091545f63a
blbecheli <76845670+blbecheli@users.noreply.github.com>
2022-08-17 20:53:25 -0300

    Projeto final

--- script.js
+++ script.js
@@ -17,11 +17,14 @@ cardName.addEventListener('focusout', (evento) => {
     const name = document.querySelector('.card__name')
     const mis__name = document.querySelector('.mis__name')
     if (cardName.value.length === 0) {
-        mis__name.classList.remove('ative')
-        cN = 0           
+        document.querySelector('.name').classList.add('wrong'); 
+        mis__name.classList.remove('ative');    
+        name.innerHTML = "";  
+        cN = 0                  
     } else {
         mis__name.classList.add('ative')
-        name.innerHTML = evento.target.value
+        name.innerHTML = evento.target.value;
+        document.querySelector('.name').classList.remove('wrong') 
         cN = 1
     }
 })
@@ -29,11 +32,14 @@ cardName.addEventListener('focusout', (evento) => {
 cardNumber.addEventListener('focusout', (evento) => {
     const number = document.querySelector('.card__number')
     const mis__number = document.querySelector(".mis__number");
-    if (cardNumber.value.length != 12) {
+    if (cardNumber.value.length != 16) {
+        document.querySelector('.card').classList.add('wrong')
         mis__number.classList.remove('ative');
+        number.innerHTML = "";
         cNu = 0;
     } else {
-        number.innerHTML = evento.target.value.replace(/^(\d{4})(\d{4})(\d{4})(\d{4})/, "$1 $2 $3 $4");
+        number.innerHTML = evento.target.value.replace(/(\d{4})(\d{4})(\d{4})(\d{4})/, "$1 $2 $3 $4");
+        document.querySelector('.card').classList.remove('wrong')
         mis__number.classList.add('ative');
         cNu = 1;
     }
@@ -49,10 +55,15 @@ cardYy.addEventListener('focusout', (evento) => {
     mm === "" ? eM = 0 : eM = 1;
     cardYy.value.length === 0 ? eY = 0 : eY = 1;
     if (mm === "" || cardYy.value.length === 0) {
-        mY.classList.remove('ative')
+        mY.classList.remove('ative');
+        document.querySelector('.mm').classList.add('wrong');
+        document.querySelector('.yy').classList.add('wrong');
+        yy.innerHTML = "";
     } else {
         mY.classList.add('ative');
-        yy.innerHTML = `${mm}/${evento.target.value}`
+        yy.innerHTML = `${mm}/${evento.target.value}`;
+        document.querySelector('.mm').classList.remove('wrong');
+        document.querySelector('.yy').classList.remove('wrong');
     }
 
 })
@@ -60,12 +71,15 @@ cardYy.addEventListener('focusout', (evento) => {
 cardCvc.addEventListener('focusout', (evento) => {
     const cvc = document.querySelector('.card__cvc');
     const mis__cvc = document.querySelector('.mis__cvc');
-    if (cardCvc.value.length === 0) {
-        mis__cvc.classList.remove('ative')
+    if (cardCvc.value.length < 3) {
+        mis__cvc.classList.remove('ative');
+        document.querySelector('.cvc__input').classList.add("wrong")
+        cvc.innerHTML = "";
         cv = 0;
     } else {
         mis__cvc.classList.add('ative');
-        cvc.innerHTML = evento.target.value
+        cvc.innerHTML = evento.target.value;
+        document.querySelector('.cvc__input').classList.remove("wrong")
         cv = 1
     }
 })
